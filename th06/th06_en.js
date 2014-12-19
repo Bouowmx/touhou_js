9f708822eaab01b372cf95fc1c1a6860a593bb69
Sean Yip <bouowmx@gmail.com>
2014-12-18 23:01:04 -0500

    Remove draw_player because the player has to be drawn every frame because the canvas is cleared after each frame

--- th06/th06_en.js
+++ th06/th06_en.js
@@ -2,12 +2,12 @@ var bullets = [];
 var canvas = $("#touhou")[0];
 var context_2d = canvas.getContext("2d");
 var draw_player = 1;
-var player_images = [];
+var player = {images: [], x: 300, y: 400};
 for (i = 0; i <= 3; i++) {
-	player_images[i] = new Image();
-	player_images[i].src = "player/reimu_" + i + ".png";
+	player.images[i] = new Image();
+	player.images[i].src = "player/reimu_" + i + ".png";
 }
-var player_sprite = 0;
+var player_image = 0;
 var shoot = 1;
 var time_g = $.now();
 
@@ -25,8 +25,8 @@ function Bullet(image, x, y, velocity_x, velocity_y, acceleration_x, acceleratio
 
 var frame = function() {
 	/*var player = $("#player")[0];
-	player.src = "player/reimu_" + player_sprite++ + ".png";
-	if (player_sprite > 3) {player_sprite = 0;}
+	player.src = "player/reimu_" + player_image++ + ".png";
+	if (player_image > 3) {player_image = 0;}
 	player.style.left = "100px";
 	player.style.top = "100px";*/
 	context_2d.clearRect(0, 0, canvas.width, canvas.height);
@@ -44,11 +44,8 @@ var frame = function() {
 			i--;
 		}
 	}
-	if (draw_player == 1) {
-		context_2d.drawImage(player_images[player_sprite++], 300, 400);
-		if (player_sprite > 3) {player_sprite = 0;}
-		draw_player = 1;
-	} else {draw_player--;}
+	context_2d.drawImage(player.images[player_image++], player.x, player.y);
+	if (player_image > 3) {player_image = 0;}
 	/*var time = $.now();
 	console.log(time - time_g);
 	time_g = time;*/
@@ -62,7 +59,7 @@ $(document).on("keydown", function(e) {
 	if (e.which == 90) {
 		if (shoot <= 1) {
 			bullets[bullets.length] = new Bullet("player/reimu_shot.png", 300, 400, 0, -5, 0, 0);
-			shoot = 5;
+			shoot = 4;
 		}
 	}
 });
